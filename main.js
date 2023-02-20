1519e1437e8b676b502d3e609692bd819488de91
Vũ Anh Tuấn <jun@KenMac.local>
2023-02-20 16:29:28 +0700

    card

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ var menu = document.querySelector('.menu__img');
 var menus = document.querySelector('.menus');
 
 menu.onclick = function(){
-    menus.classList.toggle("hide-on-m")
+    menus.style.transition = ' all 0.5s ease-in-out'
+    menus.classList.toggle("menus--active")
 }
