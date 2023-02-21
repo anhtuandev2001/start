6594be6b2b523e9484db401f93be0aa0952409f6
Vũ Anh Tuấn <jun@KenMac.local>
2023-02-21 11:04:31 +0700

    update code

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var menuImg = document.querySelector('.menu__img');
 menuMobile.onclick = function(){
     var menuText =  menus.innerHTML;
     menuMobileList.innerHTML = menuText;
-    menuMobileList.style.transition = ' all 0.5s ease-in-out'
+    menuMobileList.style.transition = ' all 0.3s ease-in-out'
     menuMobileList.classList.toggle("menu__mobile-list--active")
     iconClose.classList.toggle("hide-on-m")
     menuImg.classList.toggle("hide-on-m")
